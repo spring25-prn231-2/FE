d09d9357f58c1180a1fa23151f7a69de26ec79de
Johnny Pham <johnnypham14112003@gmail.com>
2025-01-25 23:23:37 +0700

    Add Template Bootstrap

---
+++ ChillLancer_RazorPage/ChillLancer_RazorPage/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChillLancer_RazorPage.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ChillLancer_RazorPage/ChillLancer_RazorPage/Pages/Starter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChillLancer_RazorPage.Pages
+{
+    public class StarterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

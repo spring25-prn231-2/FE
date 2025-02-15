49735006e6c9ce04e4497e27b5f3c0b0be9cbb08
Johnny Pham <johnnypham14112003@gmail.com>
2025-02-15 07:53:48 +0700

    add Dashboard Page, Authentication Page

---
+++ ChillLancer_RazorPage/ChillLancer_RazorPage/Pages/Authentication.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChillLancer_RazorPage.Pages
+{
+    public class AuthenticationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

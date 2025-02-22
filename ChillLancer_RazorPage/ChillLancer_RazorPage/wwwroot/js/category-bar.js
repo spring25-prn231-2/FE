89e9b6e6ff1e247d9fb30ae942ef7e63451c6ec8
Johnny Pham <johnnypham14112003@gmail.com>
2025-02-22 13:52:56 +0700

    Fix UI Category NavBar

--- ChillLancer_RazorPage/ChillLancer_RazorPage/wwwroot/js/category-bar.js
+++ ChillLancer_RazorPage/ChillLancer_RazorPage/wwwroot/js/category-bar.js
@@ -1,24 +1,25 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
-    const wrapper = document.querySelector('.nav-items-wrapper');
-    const navItems = document.querySelector('.nav-items');
-    const leftArrow = document.querySelector('.left-arrow');
-    const rightArrow = document.querySelector('.right-arrow');
-    const itemWidth = 200; // Chiều rộng ước tính của mỗi nav-item
+    const wrapper = document.querySelector('.categories-navbar');
+    const listItems = document.querySelector('.categories-list');
+    const leftScrollBtn = document.querySelector('.left-arrow');
+    const rightScrollBtn = document.querySelector('.right-arrow');
+    const itemWidth = 200;
 
+    //Update display arrow
     function updateArrows() {
-        const maxScroll = navItems.scrollWidth - wrapper.clientWidth;
-        leftArrow.classList.toggle('d-none', wrapper.scrollLeft <= 0);
-        rightArrow.classList.toggle('d-none', wrapper.scrollLeft >= maxScroll);
+        const maxscroll = listItems.scrollWidth - wrapper.clientWidth;
+        leftScrollBtn.classList.toggle('d-none', wrapper.scrollLeft <= 0);
+        rightScrollBtn.classList.toggle('d-none', wrapper.scrollLeft >= maxscroll);
     }
 
-    rightArrow.addEventListener('click', () => {
+    rightScrollBtn.addEventListener('click', () => {
         wrapper.scrollBy({
             left: itemWidth,
             behavior: 'smooth'
         });
     });
 
-    leftArrow.addEventListener('click', () => {
+    leftScrollBtn.addEventListener('click', () => {
         wrapper.scrollBy({
             left: -itemWidth,
             behavior: 'smooth'

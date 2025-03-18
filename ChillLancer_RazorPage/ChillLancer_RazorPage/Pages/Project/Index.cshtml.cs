a321844d79b8b6515ee08bc86b694098baf98f39
Johnny Pham <johnnypham14112003@gmail.com>
2025-03-19 03:52:02 +0700

    Fix Error after merge

--- ChillLancer_RazorPage/ChillLancer_RazorPage/Pages/Project/Index.cshtml.cs
+++ ChillLancer_RazorPage/ChillLancer_RazorPage/Pages/Project/Index.cshtml.cs
@@ -32,8 +32,8 @@ namespace ChillLancer_RazorPage.Pages.Project
         public IList<ProjectModel> Projects { get; set; } = new List<ProjectModel>();
         public List<ProjectVM> ProjectList { get; set; } = new List<ProjectVM>();
         [BindProperty]
-            public string EmployerId { get; set; }
-            public string EmployerName { get; set; }
+        public string EmployerId { get; set; }
+        public string EmployerName { get; set; }
 
         //=================[ Methods Handle Frontend Action ]=================
         public async Task OnGetAsync(string categoryName)
@@ -45,10 +45,9 @@ namespace ChillLancer_RazorPage.Pages.Project
 
             //int? customerId = _httpContextAccessor.HttpContext?.Session.GetInt32("CustomerId");
             string EmpId = _httpContextAccessor.HttpContext?.Session.GetString("EmpId") ?? "";
-                EmployerId = EmpId.Trim();
-                string EmpName = _httpContextAccessor.HttpContext?.Session.GetString("EmpName") ?? "";
-                EmployerName = EmpName.Trim();
-                string requestUrl = string.Empty;
+            EmployerId = EmpId.Trim();
+            string EmpName = _httpContextAccessor.HttpContext?.Session.GetString("EmpName") ?? "";
+            EmployerName = EmpName.Trim();
 
             string requestUrl = string.Empty;
 
@@ -64,15 +63,20 @@ namespace ChillLancer_RazorPage.Pages.Project
             //    default:
             //        return;
             //}
+            var result = await _httpClient.GetAsync(requestUrl);
+            Projects = result.IsSuccessStatusCode
+                ? await result.Content.ReadFromJsonAsync<List<ProjectModel>>() ?? new List<ProjectModel>()
+                : new List<ProjectModel>();
             try
             {
                 if (string.IsNullOrEmpty(categoryName))
                 {
-                    var result = await _httpClient.GetAsync(requestUrl);
-                    Projects = result.IsSuccessStatusCode
-                        ? await result.Content.ReadFromJsonAsync<List<ProjectModel>>() ?? new List<ProjectModel>()
-                        : new List<ProjectModel>();
-
+                    var response = await _httpClient.GetFromJsonAsync<List<ProjectVM>>($"{apiUrl}");
+                    if (response != null)
+                    {
+                        ProjectList = response;
+                    }
+                    ViewData["CategoryName"] = "ChillLancer";
                 }
                 else
                 {
